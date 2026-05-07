4a517f78914413be2533c8d538a2434560769bed
ShrekWick <nchingiz1402@gmail.com>
2026-05-07 22:22:08 +0200

    Changed color + added a small poem

---
+++ Misc/debug.py
@@ -0,0 +1,34 @@
+
+
+a = "mamma"
+a += " mia"
+print(a)
+
+
+# a1=[2,4,3]
+# a2=[5,6,4]
+
+# x1 = 0
+# x2 = 0
+# y1 = 0
+# y2 = 0
+# c1 = 1
+# c2 = 1
+
+# for y1 in range(len(a1), 0, -1):
+#     x1 += a1[y1-1] * (10**(len(a1)-c1))
+#     c1 += 1
+
+# for y2 in range(len(a2), 0, -1):
+#     x2 += a2[y2-1] * (10**(len(a2)-c2))
+#     c2 += 1
+
+# x3 = str(x1 + x2)
+
+# y3 = 0
+# l3 = []
+
+# for y3 in range(len(x3), 0, -1):
+#     l3.append(int(x3[y3 - 1]))
+
+# print(l3)
